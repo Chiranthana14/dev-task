ce1fb82d1f98a8c77109abe42ec7ac6f647fbbf1
Chiranthana14 <52007079+Chiranthana14@users.noreply.github.com>
2024-02-15 15:25:22 +0530

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include"math.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+int result;
+
+result = math::add(1,2);
+cout<<"add 1 + 2 : " << result << endl;
+
+result = math::
