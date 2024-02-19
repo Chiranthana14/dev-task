35eba4667d4744f08a0dc22a653afcecae0b983c
Chiranthana14 <52007079+Chiranthana14@users.noreply.github.com>
2024-02-19 16:13:48 +0530

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -6,21 +6,21 @@ using namespace std;
 int main(int argc, char **argv) {
     int result;
 
-    result = math::add(1, 2);
+    result = cal::add(1, 2);
     cout << "add 1 + 2 : " << result << endl;
 
-    result = math::add(4, 2);
+    result = cal::add(4, 2);
     cout << "add 4 + 2 : " << result << endl;
 
-    result = math::sub(1, 2);
+    result = cal::sub(1, 2);
     cout << "sub 1 - 2 : " << result << endl;
 
-    result = math::sub(4, 2);
+    result = cal::sub(4, 2);
     cout << "sub 4 - 2 : " << result << endl;
 
-    result = math::mul(1, 2);
+    result = cal::mul(1, 2);
     cout << "mul 1 * 2 : " << result << endl;
 
-    result = math::mul(4, 2);
+    result = cal::mul(4, 2);
     cout << "mul 4 * 2 : " << result << endl;
 }
